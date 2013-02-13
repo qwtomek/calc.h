92fd4f80ee3b1ba18cd515789cfd466c62814955
qwtomek <qwtomek@hotmail.com>
2013-02-13 07:12:53 -0800

    Create calc.hpp

---
+++ calc.hpp
@@ -0,0 +1,20 @@
+/* calc - deklaracje */
+/* Definicje stalych */
+#define PI          3.141592653589793 // PI
+#define PIx2        6.283185307179586 // 2PI
+#define PI2         1.570796326794897 // PI/2
+#define PI4         0.785398163397448 // PI/4
+#define e           2.718281828459045 // podstawa logarytmu naturalnego
+#define y           0.577215664901532 // stala Eulera
+#define phi         1.618033988749894 // zloty podzial
+
+/* Definicje funkcji */
+double suma(double, double);
+double roznica(double, double);
+double iloczyn(double, double);
+double iloraz(double, double);
+int reszta(int, int);
+double kwadrat(double);
+double szescan(double);
+double potega(double, int); //<cstdlib>
+double silnia(double);
